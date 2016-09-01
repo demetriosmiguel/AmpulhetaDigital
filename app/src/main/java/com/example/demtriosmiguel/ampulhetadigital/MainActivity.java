add7adc18d95c16383d74e7231e3205b0e97717a
Demétrios Miguel <demetriosmiguel@gmail.com>
2016-09-01 15:26:21 -0700

    Ajustes dos Botões-v2; Textos dos botões, juntamenteo com o metodo para serem ocultados;

--- app/src/main/java/com/example/demtriosmiguel/ampulhetadigital/MainActivity.java
+++ app/src/main/java/com/example/demtriosmiguel/ampulhetadigital/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     Chronometer chronometer;
     Button btstart, btpause, btStop, btReset;
+    TextView textoReniciar, textoIniciar, textoPausar, textoParar;
     boolean Click;
 
     @Override
@@ -55,19 +56,34 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MINUTOS", String.valueOf(calculoMinutos));
         Log.i("SEGUNDOS", String.valueOf(calculoSegundos));
 
-
-        // Declarando o tipo click como buleano como verdadeiro;
-        // Estanciando as variaveis
+        /* Declarando o tipo click como buleano como verdadeiro;
+         Estanciando as variaveis*/
 
         Click = true;
         chronometer = (Chronometer) findViewById(R.id.chronometer);
         btstart = (Button) findViewById(R.id.btstart);
-        btpause = (Button) findViewById(R.id.btpause);
+        btpause = (Button) findViewById(R.id.btpauseId);
         btStop = (Button) findViewById(R.id.btStop);
         btReset = (Button) findViewById(R.id.btReset);
 
-        // habilitando e desabilitando os botões
+        textoReniciar = (TextView) findViewById(R.id.textoReniciarId);
+        textoIniciar = (TextView) findViewById(R.id.textoIniciarId);
+        textoPausar = (TextView) findViewById(R.id.textoPausarId);
+        textoParar = (TextView) findViewById(R.id.textoPararId);
 
+        // habilitando e desabilitando os botões;
+
+        btstart.setVisibility(View.VISIBLE);
+        btpause.setVisibility(View.GONE);
+        btReset.setVisibility(View.GONE);
+        btStop.setVisibility(View.GONE);
+
+        // habilitando e desabilitando os textos dos botoes;
+
+        textoIniciar.setVisibility(View.VISIBLE);
+        textoReniciar.setVisibility(View.GONE);
+        textoPausar.setVisibility(View.GONE);
+        textoParar.setVisibility(View.GONE);
 
         // setando os botoes passando os evendos de click condicionando o mesmo  e startando o cronometro;
 
@@ -75,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-
 
                 if (Click)
                     chronometer.setBase(SystemClock.elapsedRealtime());
@@ -86,6 +101,11 @@ public class MainActivity extends AppCompatActivity {
                 btpause.setVisibility(View.VISIBLE);
                 btReset.setVisibility(View.VISIBLE);
                 btStop.setVisibility(View.VISIBLE);
+
+                textoIniciar.setVisibility(View.GONE);
+                textoReniciar.setVisibility(View.VISIBLE);
+                textoPausar.setVisibility(View.VISIBLE);
+                textoParar.setVisibility(View.VISIBLE);
 
             }
         });
@@ -102,6 +122,11 @@ public class MainActivity extends AppCompatActivity {
                 btpause.setVisibility(View.GONE);
                 btReset.setVisibility(View.VISIBLE);
                 btStop.setVisibility(View.VISIBLE);
+
+                textoIniciar.setVisibility(View.VISIBLE);
+                textoReniciar.setVisibility(View.VISIBLE);
+                textoPausar.setVisibility(View.GONE);
+                textoParar.setVisibility(View.VISIBLE);
 
             }
         });
@@ -120,6 +145,11 @@ public class MainActivity extends AppCompatActivity {
                 btReset.setVisibility(View.GONE);
                 btStop.setVisibility(View.GONE);
 
+                textoIniciar.setVisibility(View.VISIBLE);
+                textoReniciar.setVisibility(View.GONE);
+                textoPausar.setVisibility(View.GONE);
+                textoParar.setVisibility(View.GONE);
+
             }
         });
 
@@ -132,6 +162,11 @@ public class MainActivity extends AppCompatActivity {
                 btpause.setVisibility(View.VISIBLE);
                 btReset.setVisibility(View.VISIBLE);
                 btStop.setVisibility(View.VISIBLE);
+
+                textoIniciar.setVisibility(View.GONE);
+                textoReniciar.setVisibility(View.VISIBLE);
+                textoPausar.setVisibility(View.VISIBLE);
+                textoParar.setVisibility(View.VISIBLE);
 
                 Click = true;
                 chronometer.stop();

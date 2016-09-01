5aa3a482843bba81e36f6f5c48f34258ed0376f9
Daniel <wd.almada@gmail.com>
2016-08-31 23:07:52 -0300

    Fórmula para cálculo de horas, minutos e segundos, em segundos totais.

--- app/src/main/java/com/example/demtriosmiguel/ampulhetadigital/MainActivity.java
+++ app/src/main/java/com/example/demtriosmiguel/ampulhetadigital/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.widget.*;
 import android.view.*;
 
@@ -21,8 +22,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-// Declarando o tipo click como buleano como verdadeiro;
-// Estanciando as variaveis
+
+        // formula para calculo de tempo
+        /*
+            1 hora = 60 minutos = 3600 segundos
+
+            2 horas / 30 minutos / 25 segundos
+            25 + (30 * 60) + (2 * 60 * 60) = 9025 segundos totais
+
+            9025 / ((60 * 60) = 3600) =  2,5 pegar valor absoluto = 2
+
+            2 * 3600 (7200) - 9025 = 1825 / 60 = 30 pegar valor absoluto
+
+            30 * 60 = 1800 + 7200 = 9000
+         */
+
+        // entradas do usuario
+        int segundos = 25;
+        int minutos = 30;
+        int horas = 2;
+
+        // somatorio das entradas em segundos
+        long seguntosTotais = segundos + (minutos * 60) + (horas * 60 * 60);
+
+        long calculoHoras = Math.abs(seguntosTotais / 3600); // 2 horas
+        long calculoDiferencaMinutosEmSegundos = calculoHoras * 3600; // 7200
+        long calculoMinutos = Math.abs((calculoDiferencaMinutosEmSegundos - seguntosTotais) / 60); // 30 minutos
+        long calculoHorasMinutosEmSegundos = (calculoMinutos * 60) + calculoDiferencaMinutosEmSegundos; // 9000
+        long calculoSegundos = seguntosTotais - calculoHorasMinutosEmSegundos;
+
+        Log.i("HORAS", String.valueOf(calculoHoras));
+        Log.i("MINUTOS", String.valueOf(calculoMinutos));
+        Log.i("SEGUNDOS", String.valueOf(calculoSegundos));
+
+
+        // Declarando o tipo click como buleano como verdadeiro;
+        // Estanciando as variaveis
 
         Click = true;
         chronometer = (Chronometer) findViewById(R.id.chronometer);
@@ -31,31 +66,28 @@ public class MainActivity extends AppCompatActivity {
         btStop = (Button) findViewById(R.id.btStop);
         btReset = (Button) findViewById(R.id.btReset);
 
-// abiliitando e desabilitando os botões
+        // habilitando e desabilitando os botões
 
         btstart.setEnabled(true);
         btpause.setEnabled(false);
         btStop.setEnabled(false);
         btReset.setEnabled(false);
 
-
-
-// setando os botoes passando os evendos de click condicionando o mesmo  e startando o cronometro;
+        // setando os botoes passando os evendos de click condicionando o mesmo  e startando o cronometro;
 
         btstart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
-
                 btstart.setEnabled(false);
                 btpause.setEnabled(true);
                 btStop.setEnabled(true);
                 btReset.setEnabled(true);
 
-
                 if (Click)
                     chronometer.setBase(SystemClock.elapsedRealtime());
+
                 chronometer.start();
             }
         });
@@ -69,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
                 btpause.setEnabled(false);
                 btStop.setEnabled(true);
                 btReset.setEnabled(true);
-
 
                 Click = false;
                 chronometer.stop();
@@ -112,7 +143,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
 
     }
 }
